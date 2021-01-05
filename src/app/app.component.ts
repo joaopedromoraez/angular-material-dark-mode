26e0850194deaca21d852625b1c6956b8c6adb7e
João Pedro Moraes <joaopedromoraes@outlook.com>
2021-01-05 19:55:29 -0300

    Cria serviço para gerenciar o tema padrão da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TemaService } from 'src/app/services/tema.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-material-dark-mode';
+export class AppComponent implements OnInit{
+
+  constructor(private temaService: TemaService) { }
+
+  ngOnInit(){
+    this.temaService.initTheme();
+  }
+
 }
